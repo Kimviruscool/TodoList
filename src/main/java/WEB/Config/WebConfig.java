97bc7639a48f9124f8c74149f03b86c8b2cf0d9d
Kimviruscool <sinsa9122@naver.com>
2025-10-09 15:36:50 +0900

    edit

--- src/main/java/WEB/Config/WebConfig.java
+++ src/main/java/WEB/Config/WebConfig.java
@@ -19,24 +19,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // CSS 파일 처리
-        registry.addResourceHandler("/CSS/**")
-                .addResourceLocations("classpath:/static/CSS/")
-                .setCachePeriod(31536000);
-        
-        // JS 파일 처리
-        registry.addResourceHandler("/JS/**")
-                .addResourceLocations("classpath:/static/JS/")
-                .setCachePeriod(31536000);
-        
-        // 이미지 파일 처리
-        registry.addResourceHandler("/images/**")
-                .addResourceLocations("classpath:/static/images/")
-                .setCachePeriod(31536000);
-        
-        // 기타 정적 파일 처리
-        registry.addResourceHandler("/static/**")
+        // 모든 정적 파일 처리 - 우선순위 높게 설정
+        registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/static/")
-                .setCachePeriod(31536000);
+                .setCachePeriod(0)
+                .resourceChain(false);
     }
 }
