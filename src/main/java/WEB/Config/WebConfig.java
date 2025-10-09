7661b08597f184835dd2e46dfcd3cb5d8d0b5177
Kimviruscool <sinsa9122@naver.com>
2025-10-09 15:29:38 +0900

    vercel.json edit

--- src/main/java/WEB/Config/WebConfig.java
+++ src/main/java/WEB/Config/WebConfig.java
@@ -19,7 +19,23 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**")
+        // CSS 파일 처리
+        registry.addResourceHandler("/CSS/**")
+                .addResourceLocations("classpath:/static/CSS/")
+                .setCachePeriod(31536000);
+        
+        // JS 파일 처리
+        registry.addResourceHandler("/JS/**")
+                .addResourceLocations("classpath:/static/JS/")
+                .setCachePeriod(31536000);
+        
+        // 이미지 파일 처리
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("classpath:/static/images/")
+                .setCachePeriod(31536000);
+        
+        // 기타 정적 파일 처리
+        registry.addResourceHandler("/static/**")
                 .addResourceLocations("classpath:/static/")
                 .setCachePeriod(31536000);
     }
